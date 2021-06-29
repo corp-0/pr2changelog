7cd2a433f3e9ac52b4f6c44b7c0d89e303fb0245
Gilles <43683714+corp-0@users.noreply.github.com>
2021-06-29 03:53:52 -0400

    Rewrite (#27)
    * misc: update gitignore file
    
    * feat: rewrite of the application
    
    * feat: done for now
    
    * misc: cleaned up action.yml
    
    * deps: poetry stuff
    
    * this should be it
    
    * workflow

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
-import pr2changelog as cl
 from os import system
 
-changes = cl.change
-if changes.change_lines:
-    document = cl.document
+from pr2changelog.context import Context
+from pr2changelog.document import Document
+from pr2changelog.pr import PR
 
 
 def main():
-    if not changes.change_lines:
+    c = Context()
+    pr = PR(c.author, c.pr_number, c.url, c.change_token, c.body, categories=c.categories)
+    pr.parse_body()
+
+    if not pr.changes:
         print("PR has no changes worthy enough to mention in changelog, skipping :)")
         system('echo "::set-output name=generated_changelog::0"')
         return
 
-    write_new_changes()
+    Document(c.filename, pr.str_changes)
     system('echo "::set-output name=generated_changelog::1"')
-
-
-def write_new_changes():
-    for new_change in changes.change_lines:
-        print(f"Found new change: {new_change}")
-
-    for formatted_change in changes.get_changes():
-        document.add_record(formatted_change)
 
 
 if __name__ == "__main__":
--- tests/test_pr2changelog.py
+++
@@ -1,5 +0,0 @@
-from pr2changelog import __version__
-
-
-def test_version():
-    assert __version__ == '0.1.0'
