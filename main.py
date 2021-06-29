9f5aae2c06bf7119043be295f48645ffd4667e48
Andrés Riquelme <andresfranco.rs@gmail.com>
2021-06-29 03:50:48 -0400

    this should be it

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from os import system
+
 from pr2changelog.context import Context
 from pr2changelog.document import Document
 from pr2changelog.pr import PR
@@ -7,7 +9,14 @@ def main():
     c = Context()
     pr = PR(c.author, c.pr_number, c.url, c.change_token, c.body, categories=c.categories)
     pr.parse_body()
+
+    if not pr.changes:
+        print("PR has no changes worthy enough to mention in changelog, skipping :)")
+        system('echo "::set-output name=generated_changelog::0"')
+        return
+
     Document(c.filename, pr.str_changes)
+    system('echo "::set-output name=generated_changelog::1"')
 
 
 if __name__ == "__main__":
