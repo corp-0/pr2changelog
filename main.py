d95b2a0acd14943d4b45e191c4e63412a04bc54e
Andrés Franco Riquelme Sandoval <andresfranco.rs@gmail.com>
2022-09-10 15:04:46 -0400

    chore: try to debug change detection

--- main.py
+++ main.py
@@ -14,22 +14,22 @@ def main():
     if not pr.changes:
         print("PR has no changes skipping :)")
         system('echo "::set-output name=generated_changelog::0"')
+        system('echo "::set-output name=found_changes::0"')
         return
+    else:
+        system('echo "::set-output name=found_changes::1"')
 
     if c.write_to_file:
         doc = Document(c.filename, pr.str_changes)
         system('echo "::set-output name=generated_changelog::1"')
         system(f'echo "::set-output name=changelog_content::{doc.raw_text}"')
-    else:
-        for change in pr.str_changes:
-            system(f'echo "{change}"')
 
     if c.api_secret_token and c.api_url:
         system('echo "We are registering the changes in the changelog api!"')
         caller = ApiCaller(c.api_url, c.api_secret_token, pr)
         caller.post_changes()
         system('echo "::set-output name=generated_changelog::1"')
-        system(f'echo "::set-output name=changelog_content::{pr.str_changes}')
+        system(f'echo "::set-output name=changelog_content::{pr.str_changes}"')
 
 
 if __name__ == "__main__":
